527f5484ae97ebbf1771be8f9b718a819804d4b9
Pavlov Ivan <pavlov200912@gmail.com>
2020-10-06 23:01:42 +0300

    Fixes after soft-deadline: replaced err filtering with backtracking & id bug fixed

--- camtrack/corners.py
+++ camtrack/corners.py
@@ -37,11 +37,11 @@ class _CornerStorageBuilder:
 
 class FeatureDetector:
     def __init__(self):
-        self.shi_params = dict(maxCorners=1000, qualityLevel=0.075, minDistance=3, blockSize=7)
-        self.lucas_params = dict(winSize=(15, 15), maxLevel=3,
+        self.shi_params = dict(maxCorners=1500, qualityLevel=0.02, minDistance=3, blockSize=7)
+        self.lucas_params = dict(winSize=(15, 15), maxLevel=2,
                                  criteria=(cv2.TERM_CRITERIA_EPS | cv2.TERM_CRITERIA_COUNT, 10, 0.03))
-        self.update_period = 5
-        self.IQR_const = 1.5
+        self.IQR_const = 3
+        self.backtrack_threshold = 0.3
 
     @staticmethod
     def get_level_feature_size(level):
@@ -54,7 +54,7 @@ class FeatureDetector:
         # But in task, it's specified to use "proportional" to the image size dependency
         return 6 * (1 << level)
 
-    def pyramidal_detection(self, img, mask=None, depth=3):
+    def pyramidal_detection(self, img, mask=None, depth=2, is_initial_frame=False):
         """
         Construct depth layers of pyramid, calculate features in each layer.
         Features are filtered, using masks.
@@ -68,7 +68,7 @@ class FeatureDetector:
         else:
             level_mask = self.get_feature_mask(features, sizes, img.shape)
         for level in range(depth):
-            level_features = self.detect_features(level_image, level_mask) * (1 << level)
+            level_features = self.detect_features(level_image, level_mask, is_initial_frame) * (1 << level)
             feature_size = self.get_level_feature_size(level)
             features = np.concatenate((features, level_features))
             sizes = np.concatenate((sizes,
@@ -101,29 +101,22 @@ class FeatureDetector:
                               color=0)
         return mask
 
-    def detect_features(self, image, mask):
+    def detect_features(self, image, mask, is_initial_frame=False):
         """
+        :param is_initial_frame: In case of initial frame, we should lower features quality
         :param image: grayscale image
         :param mask: mask for shi-algorithm, mask[i][j] in {0, 255}
         :return: features to track in shape(-1, 2)
         """
-        features = cv2.goodFeaturesToTrack((image * 255).astype(np.uint8), mask=mask, **self.shi_params)
-        return features.reshape(-1, 2) if features is not None else np.array([]).reshape(-1, 2)
+        if is_initial_frame:
+            self.shi_params['qualityLevel'] /= 2
 
-    # TODO: fix this or remove
-    """
-    @staticmethod
-    def remove_intersections(features, sizes, img_shape):
-        mask = np.full(img_shape, 255)
-        new_mask = [True] * len(features)
-        for index, (point, radius) in enumerate(zip(features.reshape(-1, 2), sizes)):
-            first, second = int(point[1]), int(point[0])
-            # TODO: How point is bigger than image shape??
-            if first * second < 0 or first >= img_shape[0] or second >= img_shape[1] or mask[first][second] != 0:
-                new_mask[index] = False
-            mask = cv2.circle(mask, tuple(np.round(point).astype(int)), int(radius), thickness=-1, color=0)
-        return new_mask
-    """
+        features = cv2.goodFeaturesToTrack((image * 255).astype(np.uint8), mask=mask, **self.shi_params)
+
+        if is_initial_frame:
+            self.shi_params['qualityLevel'] *= 2
+
+        return features.reshape(-1, 2) if features is not None else np.array([]).reshape(-1, 2)
 
     def get_sparse_optical_flow(self, prev_image, cur_image, features):
         """
@@ -137,13 +130,23 @@ class FeatureDetector:
             flags=cv2.OPTFLOW_LK_GET_MIN_EIGENVALS,
             **self.lucas_params
         )
-        filter_status = status == 1
-        q3 = np.quantile(error, 0.75)
-        q1 = np.quantile(error, 0.25)
 
-        # Tukey's fences
-        filter_error = error < q3 + self.IQR_const * (q3 - q1)
-        filter_mask = (filter_error & filter_status).reshape(-1)
+        # Using backtracking for filtering features performs better, than error thresholds (subjectively)
+        back_features, back_status, back_error = cv2.calcOpticalFlowPyrLK(
+            (cur_image * 255).astype(np.uint8),
+            (prev_image * 255).astype(np.uint8),
+            new_features.astype('float32').reshape((-1, 1, 2)),
+            None,
+            flags=cv2.OPTFLOW_LK_GET_MIN_EIGENVALS,
+            **self.lucas_params
+        )
+
+        forward_back_distance = abs(features.reshape(-1, 1, 2) - back_features).reshape(-1, 2).max(-1)
+        back_tracked = forward_back_distance < self.backtrack_threshold
+
+        filter_status = status == 1
+
+        filter_mask = (filter_status & back_tracked.reshape(-1, 1)).reshape(-1)
 
         new_features = new_features if new_features is not None else np.array([]).reshape(-1, 2)
         return new_features, filter_mask
@@ -152,13 +155,14 @@ class FeatureDetector:
                        builder: _CornerStorageBuilder):
         image_0 = frame_sequence[0]
 
-        features, sizes = self.pyramidal_detection(image_0)
+        features, sizes = self.pyramidal_detection(image_0, is_initial_frame=True)
         ids = np.array(list(range(0, len(features))))
         corners = FrameCorners(
             ids,
             features,
             sizes
         )
+        max_corner_id = len(features)
         builder.set_corners_at_frame(0, corners)
 
         for frame, image_1 in enumerate(frame_sequence[1:], 1):
@@ -169,13 +173,13 @@ class FeatureDetector:
             tracked_features = tracked_features[error_mask]
             sizes = sizes[error_mask]
 
-            if frame % self.update_period == 0:
-                new_features, new_sizes = self.pyramidal_detection(image_1, mask)
-                max_id = ids.max()
-                new_ids = np.arange(max_id + 1, max_id + 1 + len(new_features))
-                ids = np.concatenate((ids, new_ids))
-                tracked_features = np.concatenate((tracked_features, new_features.reshape(-1, 1, 2)))
-                sizes = np.concatenate((sizes, new_sizes))
+            new_features, new_sizes = self.pyramidal_detection(image_1, mask)
+            new_ids = np.arange(max_corner_id, max_corner_id + len(new_features))
+            max_corner_id = max_corner_id + len(new_features)
+
+            ids = np.concatenate((ids, new_ids))
+            tracked_features = np.concatenate((tracked_features, new_features.reshape(-1, 1, 2)))
+            sizes = np.concatenate((sizes, new_sizes))
 
             corners = FrameCorners(
                 ids,
