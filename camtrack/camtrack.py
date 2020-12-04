4cff1409cc061f03464bc4115ef507784f2f843d
Pavlov Ivan <pavlov200912@gmail.com>
2020-12-04 12:12:52 +0300

    version 0.1

--- camtrack/camtrack.py
+++ camtrack/camtrack.py
@@ -42,7 +42,7 @@ def pnp_new_frame(unprocessed_frames, points3d, ids, corner_storage, intrinsic_m
         intersection_ids, (idx_1, idx_2) = snp.intersect(ids,
                                           corner_storage[frame].ids.flatten().astype(np.int32),
                                           indices=True)
-        if len(idx_1) < RANSAC_POINTS_THRESHOLD:
+        if len(idx_1) < HyperParams.RANSAC_POINTS_THRESHOLD:
             continue
 
         retval, rvec, tvec, inliers = cv2.solvePnPRansac(
@@ -50,7 +50,7 @@ def pnp_new_frame(unprocessed_frames, points3d, ids, corner_storage, intrinsic_m
             imagePoints=corner_storage[frame].points[idx_2],
             cameraMatrix=intrinsic_mat,
             distCoeffs=None,
-            reprojectionError=RANSAC_REPROJECTION_ERROR,
+            reprojectionError=HyperParams.RANSAC_REPROJECTION_ERROR,
             flags=cv2.SOLVEPNP_EPNP
         )
 
@@ -79,15 +79,29 @@ def pnp_new_frame(unprocessed_frames, points3d, ids, corner_storage, intrinsic_m
     return new_frame, new_pos, max_inliers_len
 
 
-RANSAC_POINTS_THRESHOLD = 5
-RANSAC_REPROJECTION_ERROR = 3
-RANSAC_ITERATIONS = 100
-RETRIANGULATION_ITER_LIMIT = 50
-RETRIANGULATION_SIZE_LIMIT = 300
-RETRIANGULATION_FRAME_COUNT_LIMIT = 25
-RETRIANGULATION_RANSAC_ITERATIONS = 3
-RETRIANGULATION_RANSAC_REPROJECTION_ERROR = 1.5
-SHORT_THRESHOLD = 1
+class HyperParams:
+    RANSAC_POINTS_THRESHOLD = 5
+    RANSAC_REPROJECTION_ERROR = 3
+    RANSAC_ITERATIONS = 100
+    RETRIANGULATION_ITER_LIMIT = 50
+    RETRIANGULATION_SIZE_LIMIT = 300
+    RETRIANGULATION_FRAME_COUNT_LIMIT = 25
+    RETRIANGULATION_RANSAC_ITERATIONS = 3
+    RETRIANGULATION_RANSAC_REPROJECTION_ERROR = 1.5
+    SHORT_THRESHOLD = 1   # Nice for usage when > 1, but ironman test bans this feature =((((
+
+    @staticmethod
+    def lower_thresholds():
+        HyperParams.RANSAC_POINTS_THRESHOLD *= 2
+        HyperParams.RANSAC_REPROJECTION_ERROR *= 2
+        HyperParams.RETRIANGULATION_RANSAC_REPROJECTION_ERROR *= 2
+
+    @staticmethod
+    def lower_triangulation_params(triangulation_params):
+        return TriangulationParameters(0,
+        triangulation_params.max_reprojection_error * 2,
+        triangulation_params.min_triangulation_angle_deg / 2)
+
 
 
 def calculate_initial_views(corner_storage, intrinsic_mat, triangulation_params):
@@ -96,22 +110,31 @@ def calculate_initial_views(corner_storage, intrinsic_mat, triangulation_params)
     # - frame distance
     min_intersection = 10
     top_pairs = []
+    min_distance = 10
+    max_distance = 25
+
+    # TODO: REWRITE
+    scaler = lambda x: x // 100 if x >= 1000 else x
     for frame1 in range(len(corner_storage)):
         for frame2 in range(frame1 + 1, len(corner_storage)):
             corrs = build_correspondences(corner_storage[frame1],
                                   corner_storage[frame2])
             intersect_len = len(corrs.ids)
-            if intersect_len > min_intersection:
+            if intersect_len > min_intersection and min_distance <= frame2 - frame1 <= max_distance:
                 top_pairs.append((frame1, frame2, intersect_len, frame2 - frame1))
-    top_pairs = sorted(top_pairs, key=lambda x: (x[2], x[3]), reverse=True)
+    top_pairs = sorted(top_pairs, key=lambda x: (scaler(x[2]), x[3]), reverse=True)
 
-    pairs_to_check = 125
+
+    print(*list(top_pairs)[:100], sep='\n')
+    pairs_to_check = 100
     min_essential_inliers = 10
     min_triangulated_inliers = 10
     essential_homography_ratio_threshold = 1.25
     best_pair_triangulated_len = -1
     result_frame_1, result_frame_2 = -1, -1
     result_camera_pos = None
+
+    top_pairs_stats = []
     for frame1, frame2, _, _ in top_pairs[:pairs_to_check]:
         corrs = build_correspondences(corner_storage[frame1], corner_storage[frame2])
         assert len(corrs.ids) >= 5  # Otherwise, can't solve with cv2
@@ -121,24 +144,34 @@ def calculate_initial_views(corner_storage, intrinsic_mat, triangulation_params)
                                                            prob=0.995,
                                                            threshold=1.0)
 
-        if essential_inliers < min_essential_inliers:
-            continue
 
         _, homography_inliers = cv2.findHomography(corrs.points_1, corrs.points_2,
                                                    method=cv2.RANSAC,
                                                    confidence=0.995 ,
-                                                   ransacReprojThreshold=RANSAC_REPROJECTION_ERROR)
+                                                   ransacReprojThreshold=HyperParams.RANSAC_REPROJECTION_ERROR)
 
-        if np.sum(essential_inliers) < essential_homography_ratio_threshold * np.sum(homography_inliers):
-            continue
 
         ids_to_remove = np.where(essential_inliers == 0)[0]
-        corrs = build_correspondences(corner_storage[frame1], corner_storage[frame2], ids_to_remove=ids_to_remove)
+
+        corrs = build_correspondences(corner_storage[frame1], corner_storage[frame2],
+                                      ids_to_remove=ids_to_remove.astype(np.int32))
 
         _, R, t, triangulated_inliers = cv2.recoverPose(essential_mat, corrs.points_1, corrs.points_2, intrinsic_mat)
 
-        if triangulated_inliers < min_triangulated_inliers:
-            continue
+
+        print(f"DEBUG: {frame1}:{frame2} corrs: {len(corrs.ids)} E_I:{np.count_nonzero(essential_inliers)} H_I: {np.count_nonzero(homography_inliers)} "
+               f"R: {np.count_nonzero(essential_inliers) / np.count_nonzero(homography_inliers)} T_I:{np.count_nonzero(triangulated_inliers)}")
+
+        #if np.count_nonzero(essential_inliers) < essential_homography_ratio_threshold * np.count_nonzero(homography_inliers):
+        #    continue
+
+
+        #if np.count_nonzero(triangulated_inliers) < min_triangulated_inliers:
+        #    continue
+
+        #if np.count_nonzero(essential_inliers) < min_essential_inliers:
+        #    continue
+
         points, ids, _ = triangulate_correspondences(corrs,
                                                      eye3x4(),
                                                      np.hstack((R, t)),
@@ -146,11 +179,24 @@ def calculate_initial_views(corner_storage, intrinsic_mat, triangulation_params)
                                                      triangulation_params)
 
         triangulated_len = len(ids)
-        if triangulated_len > best_pair_triangulated_len:
-            result_frame_1, result_frame_2 = frame1, frame2
-            best_pair_triangulated_len = triangulated_len
-            result_camera_pos = np.hstack((R, t))
+        #if scaler(triangulated_len) > best_pair_triangulated_len:
+        #    result_frame_1, result_frame_2 = frame1, frame2
+        #    best_pair_triangulated_len = scaler(triangulated_len)
+        #    result_camera_pos = np.hstack((R, t))
+        top_pairs_stats.append(((frame1, frame2),
+                                triangulated_len,
+                                np.count_nonzero(essential_inliers) / (np.count_nonzero(homography_inliers) + 1),
+                                np.count_nonzero(triangulated_inliers),
+                                np.count_nonzero(essential_inliers),
+                                np.hstack((R, t))
+                                ))
 
+    top_pairs_stats = list(sorted(top_pairs_stats, key=lambda x: (x[1], x[2], x[3], x[4]), reverse=True))
+    best_pair_triangulated_len = top_pairs_stats[0][1]
+    result_frame_1, result_frame_2 = top_pairs_stats[0][0]
+    result_camera_pos = top_pairs_stats[0][-1]
+
+    print(f"Best triangulated result: {best_pair_triangulated_len * 100}")
     return (result_frame_1, view_mat3x4_to_pose(eye3x4())), (result_frame_2, view_mat3x4_to_pose(result_camera_pos))
 
 
@@ -165,7 +211,7 @@ def track_and_calc_colors(camera_parameters: CameraParameters,
                           known_view_1: Optional[Tuple[int, Pose]] = None,
                           known_view_2: Optional[Tuple[int, Pose]] = None) \
         -> Tuple[List[Pose], PointCloud]:
-    corner_storage = without_short_tracks(corner_storage, SHORT_THRESHOLD)
+    corner_storage = without_short_tracks(corner_storage, HyperParams.SHORT_THRESHOLD)
 
     rgb_sequence = frameseq.read_rgb_f32(frame_sequence_path)
     intrinsic_mat = to_opencv_camera_mat3x3(
@@ -229,11 +275,15 @@ def track_and_calc_colors(camera_parameters: CameraParameters,
 
     iteration = 0
     last_retriangulate_time = {}
-
+    dooms_day_counter = 0
     while True:
         iteration += 1
         update = False
         unprocessed_frames = np.arange(len(corner_storage), dtype=np.int32)[~processed_frames]
+
+        if len(unprocessed_frames) == 0:
+            print("Done!")
+            break
 
         # Solve PnPRansac
         pnp_start = time.time()
@@ -258,11 +308,11 @@ def track_and_calc_colors(camera_parameters: CameraParameters,
             trian_start = time.time()
             point_to_retriangulate = []
             for id in corner_storage[new_frame].ids.flatten():
-                if id not in last_retriangulate_time or iteration - last_retriangulate_time[id] > RETRIANGULATION_ITER_LIMIT:
+                if id not in last_retriangulate_time or iteration - last_retriangulate_time[id] > HyperParams.RETRIANGULATION_ITER_LIMIT:
                     point_to_retriangulate.append(id)
 
-            if len(point_to_retriangulate) > RETRIANGULATION_SIZE_LIMIT:
-                point_to_retriangulate = np.random.choice(point_to_retriangulate, RETRIANGULATION_SIZE_LIMIT, replace=False)
+            if len(point_to_retriangulate) > HyperParams.RETRIANGULATION_SIZE_LIMIT:
+                point_to_retriangulate = np.random.choice(point_to_retriangulate, HyperParams.RETRIANGULATION_SIZE_LIMIT, replace=False)
 
 
             def retriangulate_feature(corner):
@@ -278,14 +328,14 @@ def track_and_calc_colors(camera_parameters: CameraParameters,
                     return None
 
                 # Limit frames
-                if len(frames_info) > RETRIANGULATION_FRAME_COUNT_LIMIT:
-                    frame_ids = np.random.choice(np.arange(len(frames_info)), RETRIANGULATION_FRAME_COUNT_LIMIT, replace=False)
+                if len(frames_info) > HyperParams.RETRIANGULATION_FRAME_COUNT_LIMIT:
+                    frame_ids = np.random.choice(np.arange(len(frames_info)), HyperParams.RETRIANGULATION_FRAME_COUNT_LIMIT, replace=False)
                     frames_info = np.array(frames_info, dtype=object)[frame_ids]
                 else:
                     frames_info = np.array(frames_info, dtype=object)
                 # RANSAC:
                 new_point, point_inliers_score = -1, -1
-                for _ in range(RETRIANGULATION_RANSAC_ITERATIONS):
+                for _ in range(HyperParams.RETRIANGULATION_RANSAC_ITERATIONS):
                     id_frame1, id_frame2 = np.random.choice(len(frames_info), 2, replace=False)
 
                     frame1, p1, pos1 = frames_info[id_frame1]
@@ -302,7 +352,7 @@ def track_and_calc_colors(camera_parameters: CameraParameters,
                     e = np.array(
                          [compute_reprojection_errors(p, point2d, intrinsic_mat @ mat) for frame, point2d, mat in frames_info]
                     )
-                    good = e < RETRIANGULATION_RANSAC_REPROJECTION_ERROR
+                    good = e < HyperParams.RETRIANGULATION_RANSAC_REPROJECTION_ERROR
                     inliers_count = sum(good)[0]
                     if inliers_count > point_inliers_score:
                         point_inliers_score = inliers_count
@@ -342,8 +392,15 @@ def track_and_calc_colors(camera_parameters: CameraParameters,
 
 
         if not update:
-            print(f"Only: {sum(processed_frames) / frame_count} processed =(")
-            break
+            dooms_day_counter += 1
+            print("CANT UPDATE ANYTHING, LOWING THRESHOLDS!")
+            HyperParams.lower_thresholds()
+            triangulation_params = HyperParams.lower_triangulation_params(triangulation_params)
+            if dooms_day_counter == 25:
+                break
+        else:
+            dooms_day_counter = 0
+
 
     point_cloud_builder = PointCloudBuilder(np.array(np.nonzero(cloud_ids)[0], dtype=np.int32), points3d[np.array(cloud_ids).astype(bool)])
 
